b19d21ceeb6b7ca285e3045a51ecea51a80c7bb2
Stv <veselinoStf@gmail.com>
2020-08-02 08:35:38 +0300

    Initial Structure for TeamService

---
+++ src/JustOrganize.TeamService/Models/Team.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustOrganize.TeamService.Models
+{
+    public class Team
+    {
+        public string Name { get; set; }
+
+        public Guid Id { get; set; }
+
+        public ICollection<Member> Members { get; set; }
+        public Team()
+        {
+            this.Members = new List<Member>();
+        }
+
+        public Team(string name):this()
+        {
+            this.Name = name;
+        }
+
+        public Team(string name, Guid id) : this(name)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
---
+++ test/JustOrganize.TeamService.Tests/TeamControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace JustOrganize.TeamService.Tests
+{
+    public class TeamControllerTests
+    {
+        [Fact]
+        public void QueryTeamListReturnsCorrectTeams()
+        {
+
+        }
+    }
+}
